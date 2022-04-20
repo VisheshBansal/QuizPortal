d4c28b618eafcff839b91a770b4508f811ee6b17
VisheshBansal <vishesh.bansal@gmail.com>
2022-04-20 15:09:05 +0530

    Deactivate CORS

--- backend/app.js
+++ backend/app.js
@@ -4,17 +4,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
-const cors = require('cors');
+//const cors = require('cors');
 const Info = require('./api/models/info');
 
 const app = express();
 var http = require('http').Server(app);
-const io = require('socket.io')(http, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
-});
+const io = require('socket.io')(http);
 
 app.use(express.json({ limit: '50mb' }));
 app.use(helmet());
@@ -36,7 +31,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(cors());
+//app.use(cors());
 
 // ADD ROUTERS
 
